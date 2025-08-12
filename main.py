fe5040637eb61b64180f9a04ace07baa18a15b9d
eudimars <eudimar84@hotmail.com>
2025-08-11 21:46:52 -0300

    Created main loop Connection Class Menu and Game Class

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pygame
+from CodeGame.Game import Game
 
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print('Setup End')
-
-print('Setup Start')
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print('Quittingrr...')
-            pygame.quit()  # Close Window
-            quit()  # End Pygame
+game = Game()
+game.run()
