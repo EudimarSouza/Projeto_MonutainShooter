8b4d90e5dc442cfd1d14982fa016549b5a5216f7
eudimars <eudimar84@hotmail.com>
2025-08-12 22:18:46 -0300

    Created Asset Folder Insert Assets Creating the Menu: background, sound and font Created Folder Const

--- CodeGame/Game.py
+++ CodeGame/Game.py
@@ -2,23 +2,20 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from CodeGame.Cost import WIN_HEIGHT
+from CodeGame.Cost import WIN_WIDTH
 from CodeGame.Menu import Menu
+
 
 class Game:
 
     def __init__(self):
         # self.window = None
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # Close Window
-            #         quit()  # End Pygame
