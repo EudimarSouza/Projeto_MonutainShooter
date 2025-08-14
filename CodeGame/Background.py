74ebe69c295cdac48130687e8c8230527fd0c227
eudimars <eudimar84@hotmail.com>
2025-08-13 23:23:11 -0300

    Creating Menu interactions creating ABC Class Creating Factory Level 1 Implementation

--- CodeGame/Background.py
+++ CodeGame/Background.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+from CodeGame.Cost import WIN_WIDTH, ENTITY_SPEED
 from CodeGame.Entity import Entity
 
 
@@ -9,4 +9,6 @@ class Background(Entity):
         super().__init__(name, position)
 
     def move(self, ):
-        pass
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
--- CodeGame/Level.py
+++ CodeGame/Level.py
@@ -12,12 +12,12 @@ class Level:
         self.name = name
         self.game_mode = game_mode  # modo de jogo
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Leve1Bg'))
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
     def run(self, ):
         while True:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
             pygame.display.flip()
-
         pass
