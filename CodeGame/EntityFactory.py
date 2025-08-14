d0ac7bf7a870423077888dab9b9b70f35b490347
eudimars <eudimar84@hotmail.com>
2025-08-13 22:48:45 -0300

    Insert all Assets

--- CodeGame/EntityFactory.py
+++ CodeGame/EntityFactory.py
@@ -11,5 +11,5 @@ class EntityFactory:
             case 'Level1Bg':
                 list_bg = []
                 for i in range(7):
-                    list_bg.append(Background('Level1Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                 return list_bg
